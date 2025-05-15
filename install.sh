abbe41a5e139f3a8caa8874b73a6570b10647b74
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-15 13:18:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,16 @@ NC='\e[0m'
 # ==== Export CREDITS
 CREDITS="${green} ـــــــــــــــﮩ٨ـ QuickTunnel ${NC}" 
 
+# Running Banner
+TEXT="QuickTunnel ـــــــــــــــﮩ٨ـ"
+for (( i=0; i<${#TEXT}; i++ ))
+do
+    echo -n -e "${green}${TEXT:$i:1}${NC}"
+    sleep 0.1 #Delay Antar Huruf
+done
+clear
+
+
 # ==== Setup Input Domain
 echo -e "┌───────────────────────────────────────────────────┐"
 echo -e "│  Setup Your Domain For This Script | ${green}QuickTunnel${NC}  │"
