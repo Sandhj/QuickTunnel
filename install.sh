fa59ebdbda6929b3c94849fa64ffa8ab1e4dd5dc
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-15 13:00:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,13 @@ NC='\e[0m'
 # ==== Export CREDITS
 CREDITS="${green} ـــــــــــــــﮩ٨ـ QuickTunnel ${NC}" 
 
+# ==== Setup Input Domain
+echo -e "┌─────────────────────────────────────────────────┐"
+echo -e "│  Setup Your Domain For This Script | $CREDITS   │"
+echo -e "└─────────────────────────────────────────────────┘"
+echo ""
+read -p "Username : " user
+read -p "Masa Aktif: " jumlah_hari
+read -p "Limit IP: " jumlah
+
 
