281e6b2519913933c53b0983ff494e50b5d48d09
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-22 12:52:28 +0800

    Update list-user-ssh.sh

--- manager-ssh/list-user-ssh.sh
+++ manager-ssh/list-user-ssh.sh
@@ -3,24 +3,17 @@ clear
 echo "---------------------------------------"
 echo "       .:: LIST SSH ACCOUNT ::.        "
 echo "---------------------------------------"
+printf "%-3s | %-17s | %-12s\n" "No" "Username" "Expired"
+echo "---------------------------------------"
 
-# Inisialisasi counter
 no=1
 
-# Baca semua user dari /etc/passwd
-cut -d: -f1 /etc/passwd | while read user
-do
-    # Dapatkan UID user
-    uid=$(id -u "$user" 2>/dev/null)
-
+getent passwd | while IFS=: read -r username pass uid gid gecos home shell; do
     # Filter hanya user biasa (UID >= 1000 dan bukan nobody/65534)
     if [[ "$uid" -ge 1000 && "$uid" -ne 65534 ]]; then
-        # Ambil tanggal expired
-        exp=$(sudo chage -l "$user" 2>/dev/null | grep "Account expires" | awk -F": " '{print $2}')
-        
-        # Jika ada informasi expired, tampilkan
+        exp=$(chage -l "$username" 2>/dev/null | grep "Account expires" | awk -F": " '{print $2}')
         if [ ! -z "$exp" ]; then
-            printf "%-3s | %-17s | %-12s\n" "$no" "$user" "$exp"
+            printf "%-3s | %-17s | %-12s\n" "$no" "$username" "$exp"
             no=$((no + 1))
         fi
     fi
