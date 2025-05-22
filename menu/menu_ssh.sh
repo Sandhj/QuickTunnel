60397baebf16bc38327cb5a9ba4880adddcdc826
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-22 11:46:52 +0800

    Update menu_ssh.sh

--- menu/menu_ssh.sh
+++ menu/menu_ssh.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 clear
-
 # Definisi Warna
 BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
-
 echo -e "${BLUE}   ┌──────────────────────────────────────┐${NC}"
-echo -e "${BLUE}   │       .::   MENU SSH MANAGER  ::.        │${NC}"
+echo -e "${BLUE}   |      .::   MENU SSH MANAGER  ::.     │${NC}"
 echo -e "${BLUE}   └──────────────────────────────────────┘${NC}"
-echo -e "    1. Create New Account         "
-echo -e "    2. Renew Account"
-echo -e "    3. Delete Account       "
-echo -e "    4. List Account                "
-echo -e "    5. Cek Login          "
+echo -e "      1. Create New Account         "
+echo -e "      2. Renew Account"
+echo -e "      3. Delete Account       "
+echo -e "      4. List Account                "
+echo -e "      5. Cek Login          "
 echo -e "${BLUE}   └──────────────────────────────────────┘${NC}"
 echo -e ""
 read -p "Select Menu : " pilihan
-
 # Memproses pilihan
 case $pilihan in
     1)
