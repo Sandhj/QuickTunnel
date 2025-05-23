7590ebb59180fa52b0f7c3cd597d7c17bf00606c
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-23 10:26:17 +0800

    Create menu_vmess.sh

---
+++ menu/menu_vmess.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+# Definisi Warna
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+echo -e "${BLUE}   ┌──────────────────────────────────────┐${NC}"
+echo -e "${BLUE}   |      .::   MENU VMESS MANAGER  ::.   │${NC}"
+echo -e "${BLUE}   └──────────────────────────────────────┘${NC}"
+echo -e "      1. Create New Account              "
+echo -e "      2. Renew Account                   "
+echo -e "      3. Delete Account                  "
+echo -e "      4. List Account                    "
+echo -e "      5. Cek Login                       "
+echo -e "      6. Trial Account 60 Menit          "
+echo -e "${BLUE}   └──────────────────────────────────────┘${NC}"
+echo -e ""
+read -p "Select Menu (0 To Back Menu) : " pilihan
+# Memproses pilihan
+case $pilihan in
+    1)
+        ./add-vmess.sh
+        ;;
+    2)
+        ./renew-vmess.sh
+        ;;
+    3)
+        ./delete-vmess.sh
+        ;;
+    4)
+        ./list_user_vmess
+        ;;
+    5)
+        ./trial_vmess
+        ;;
+    0)
+        ./menu.sh
+    *)
+        ./menu_vmess
+        ;;
+esac
