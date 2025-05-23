7f9f81f7c906fd1511220096b1353f5232875bdb
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-23 13:07:28 +0800

    Create menu_lain.sh

---
+++ menu/menu_lain.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+# Definisi Warna
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+echo -e "${BLUE}   ┌──────────────────────────────────────┐${NC}"
+echo -e "${BLUE}   |      .::   MENU TOOLS MANAGER  ::.   │${NC}"
+echo -e "${BLUE}   └──────────────────────────────────────┘${NC}"
+echo -e "      1. Cert Domain              "
+echo -e "      2. Change Domain                  "
+echo -e "      3. Vnstat                  "
+echo -e "      4. Status Service                   "
+echo -e "      5. Reboot Server          "
+echo -e "${BLUE}   └──────────────────────────────────────┘${NC}"
+echo -e ""
+read -p "Select Menu (0 To Back Menu) : " pilihan
+# Memproses pilihan
+case $pilihan in
+    1)
+        ./cert-domain.sh
+        ;;
+    2)
+        ./change-domain.sh
+        ;;
+    3)
+        ./vnstat
+        ;;
+    4)
+        ./status-service.sh
+        ;;
+    5)
+        ./reboot
+        ;;
+    0)
+        ./menu.sh
+        ;;
+    *)
+        ./menu_lain.sh
+        ;;
+esac
