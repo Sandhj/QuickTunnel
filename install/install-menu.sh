dc1cb7be3168c9dd180d45f939529c644db0e070
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-23 13:33:18 +0800

    Update install-menu.sh

--- install/install-menu.sh
+++ install/install-menu.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 URL="https://raw.githubusercontent.com/Sandhj/QuickTunnel/main/menu"
 
-sudo wget -q -O /usr/bin/menu "$URL/menu.sh" && sudo chmod +x /usr/bin/menu
-sudo wget -q -O /usr/bin/menu_bot "$URL/menu_bot.sh" && sudo chmod +x /usr/bin/menu_bot
-sudo wget -q -O /usr/bin/menu_lain "$URL/menu_lain.sh" && sudo chmod +x /usr/bin/menu_lain
-sudo wget -q -O /usr/bin/menu_ssh "$URL/menu_ssh.sh" && sudo chmod +x /usr/bin/menu_ssh
-sudo wget -q -O /usr/bin/menu_vmess "$URL/menu_vmess.sh" && sudo chmod +x /usr/bin/menu_vmess
-sudo wget -q -O /usr/bin/menu_vless "$URL/menu_vless.sh" && sudo chmod +x /usr/bin/menu_vless
-sudo wget -q -O /usr/bin/menu_trojan "$URL/menu_trojan.sh" && sudo chmod +x /usr/bin/menu_trojan
+sudo wget -q -O /usr/bin/menu.sh "$URL/menu.sh" && sudo chmod +x /usr/bin/menu.sh
+sudo wget -q -O /usr/bin/menu_bot.sh "$URL/menu_bot.sh" && sudo chmod +x /usr/bin/menu_bot.sh
+sudo wget -q -O /usr/bin/menu_lain.sh "$URL/menu_lain.sh" && sudo chmod +x /usr/bin/menu_lain.sh
+sudo wget -q -O /usr/bin/menu_ssh.sh "$URL/menu_ssh.sh" && sudo chmod +x /usr/bin/menu_ssh.sh
+sudo wget -q -O /usr/bin/menu_vmess.sh "$URL/menu_vmess.sh" && sudo chmod +x /usr/bin/menu_vmess.sh
+sudo wget -q -O /usr/bin/menu_vless.sh "$URL/menu_vless.sh" && sudo chmod +x /usr/bin/menu_vless.sh
+sudo wget -q -O /usr/bin/menu_trojan.sh "$URL/menu_trojan.sh" && sudo chmod +x /usr/bin/menu_trojan.sh
