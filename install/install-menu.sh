d3b4cfbc371d0cd1aad70d22165a2f488769fb25
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-23 13:31:13 +0800

    Update install-menu.sh

--- install/install-menu.sh
+++ install/install-menu.sh
@@ -1,1 +1,9 @@
-y
+URL="https://raw.githubusercontent.com/Sandhj/QuickTunnel/main/menu"
+
+sudo wget -q -O /usr/bin/menu "$URL/menu.sh" && sudo chmod +x /usr/bin/menu
+sudo wget -q -O /usr/bin/menu_bot "$URL/menu_bot.sh" && sudo chmod +x /usr/bin/menu_bot
+sudo wget -q -O /usr/bin/menu_lain "$URL/menu_lain.sh" && sudo chmod +x /usr/bin/menu_lain
+sudo wget -q -O /usr/bin/menu_ssh "$URL/menu_ssh.sh" && sudo chmod +x /usr/bin/menu_ssh
+sudo wget -q -O /usr/bin/menu_vmess "$URL/menu_vmess.sh" && sudo chmod +x /usr/bin/menu_vmess
+sudo wget -q -O /usr/bin/menu_vless "$URL/menu_vless.sh" && sudo chmod +x /usr/bin/menu_vless
+sudo wget -q -O /usr/bin/menu_trojan "$URL/menu_trojan.sh" && sudo chmod +x /usr/bin/menu_trojan
