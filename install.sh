0f9b8864cfc688d58b9aec337a058fc2c7f6f7a9
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-24 16:12:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,6 +51,11 @@ wget -q ${GITHUB}install/install-xray.sh && bash install-xray.sh
 wget -q ${GITHUB}install/install-menu.sh && bash install-menu.sh
 # ==== Install Vnstat
 wget -q ${GITHUB}install/install-vnstat.sh && bash install-vnstat.sh
+# Install Limit IP Xray
+cd /etc/xray/
+wget -q ${GITHUB}tools/check_ip_limit.sh 
+wget -q ${GITHUB}tools/clients_limit.conf 
+cd
 
 # ==== Memasang Default Menu saat Boot
 cat> /root/.profile << END
