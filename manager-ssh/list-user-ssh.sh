27d168e754ce160c02b55beed11d1d8b56d2c048
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-24 10:30:22 +0800

    Update list-user-ssh.sh

--- manager-ssh/list-user-ssh.sh
+++ manager-ssh/list-user-ssh.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 clear
-echo "---------------------------------------"
-echo "       .:: LIST SSH ACCOUNT ::.        "
-echo "---------------------------------------"
-printf "%-3s | %-17s | %-12s\n" "No" "Username" "Expired"
-echo "---------------------------------------"
-
+echo -e "  ┌──────────────────────────────────────┐"
+echo -e "  │       .:: LIST SSH ACCOUNT ::.       │"
+echo -e "  └──────────────────────────────────────┘"
+printf "   %-2s | %-17s | %-12s\n" "No" "  Username" "Expired"
+echo "  ---------------------------------------"
 no=1
-
 getent passwd | while IFS=: read -r username pass uid gid gecos home shell; do
     # Filter hanya user biasa (UID >= 1000 dan bukan nobody/65534)
     if [[ "$uid" -ge 1000 && "$uid" -ne 65534 ]]; then
-        exp=$(chage -l "$username" 2>/dev/null | grep "Account expires" | awk -F": " '{print $2}')
+        exp=$(chage -l "$username" 2>/dev/null | grep "Account expires" | awk -F": " '>
         if [ ! -z "$exp" ]; then
-            printf "%-3s | %-17s | %-12s\n" "$no" "$username" "$exp"
+            printf "   %-2s | %-17s | %-12s\n" "$no" "$username" "$exp"
             no=$((no + 1))
         fi
     fi
 done
-
-echo "---------------------------------------"
+echo "  ---------------------------------------"
 echo "Tekan Enter Untuk Kembali (↩️)"
 read -s
 menu
