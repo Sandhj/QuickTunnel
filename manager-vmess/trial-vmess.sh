4bcacbb7641e3302376fa6f9ad266e040dd0f44d
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-24 09:50:27 +0800

    Update trial-vmess.sh

--- manager-vmess/trial-vmess.sh
+++ manager-vmess/trial-vmess.sh
@@ -4,7 +4,7 @@ export RANDOM_HURUF
 
 clear
 echo -e "┌──────────────────────────────────────┐"
-echo -e "│   =   CREATE NEW VMESS ACCOUNT   =   │"
+echo -e "│   =   CREATE TRIAL VMESS ACCOUNT   = │"
 echo -e "└──────────────────────────────────────┘"
 echo ""
 user=Trial-$RANDOM_HURUF
