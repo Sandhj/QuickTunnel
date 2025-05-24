7a52ef06f2172739d0c7d9a1b74c3ca1da21c060
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-24 15:51:11 +0800

    Update install-autobackup.sh

--- install/install-autobackup.sh
+++ install/install-autobackup.sh
@@ -6,5 +6,6 @@ GITHUB="https://raw.githubusercontent.com/Sandhj/QuickTunnel/main/"
 # Install autobackup l
 wget -q ${GITHUB}tools/auto_backup.sh && bash auto_backup.sh
 
+cd
 # Install LimitIP
 wget -q ${GITHUB}tools/limit_ip.sh && bash limit_ip.sh
