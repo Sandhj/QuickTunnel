557b3e20c930ae54f9e24f8c4e10934c41cb5775
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-24 13:27:16 +0800

    Update change-domain.sh

--- tools/change-domain.sh
+++ tools/change-domain.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # ==== Setup Input Domain
 echo -e "┌───────────────────────────────────────────────────┐"
 echo -e "│  Setup New Domain For This Script | ${green}QuickTunnel${NC}   │"
@@ -6,3 +8,19 @@ echo ""
 read -p "Type Your Domain : " domain
 
 echo $domain >> /root/domain
+
+# ==== hapus cert lama
+rm -f /etc/xray/xray.crt
+rm -f /etc/xray/xray.key
+
+# ==== Cert Domain Dengan Acme
+systemctl stop nginx
+mkdir /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+
+rm /root/domain
