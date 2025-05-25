d3e4fdc0b12e6555456f2678d5408f58721d6410
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-25 13:42:45 +0800

    Update install-autobackup.sh

--- install/install-autobackup.sh
+++ install/install-autobackup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Setup Data Bot
-read -p "NAMA SERVER : " name_server
-read -p "BOT TOKEN   : " token
-read -p "CHAT ID     : " chatid
+# Minta input dari pengguna
+read -p "Masukkan token : " new_token
+read -p "Masukkan chat : " new_chatid
+read -p "Masukkan nama server : " new_name
 
-echo $name_server >> /etc/xray/name_server
-echo $token >> /etc/xray/token
-echo $chatid >> /etc/xray/chatid
+# Ganti nilai di file Python dengan perlindungan karakter khusus
+sed -i 's/^bot_token\s*=\s*".*"/bot_token = "'"$new_token"'"/' /opt/autobackup/auto.py
+sed -i 's/^chat_id\s*=\s*".*"/chat_id = "'"$new_chatid"'"/' /opt/autobackup/auto.py
+sed -i 's/^name_server\s*=\s*".*"/name_server = "'"$new_name"'"/' /opt/autobackup/auto.py
+
+echo $new_token >> /etc/xray/token
+echo $new_chatid >> /etc/xray/chatid
 
 systemctl restart auto
+
+echo " ✅ Autobackup Berhasil di Pasang. . .!"
