610f70872d05ad6ad4b440a5cb781b43e20a867c
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-25 18:13:47 +0800

    Update instal-update.sh

--- install/instal-update.sh
+++ install/instal-update.sh
@@ -1,2 +1,9 @@
-# ==== Update SSH
-https://raw.githubusercontent.com/Sandhj/QuickTunnel/main/
+# ==== Link Github
+GITHUB="https://raw.githubusercontent.com/Sandhj/QuickTunnel/main"
+
+# VMESS
+/manager-ssh/trial-ssh.sh
+/manager-ssh/renew-ssh.sh
+/manager-ssh/list-user-ssh.sh
+/manager-ssh/add-ssh.sh
+/manager-ssh/delete-ssh.sh
