f972eff0b62b0b741721f7fac0f42b0912d61d18
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-26 17:44:27 +0800

    Update install-autobackup.sh

--- install/install-autobackup.sh
+++ install/install-autobackup.sh
@@ -3,12 +3,12 @@
 # Minta input dari pengguna
 read -p "Masukkan token : " new_token
 read -p "Masukkan chat : " new_chatid
-read -p "Masukkan nama server : " new_name
+read -p "Masukkan nama server : " NEW_NAME
 
 # Ganti nilai di file Python dengan perlindungan karakter khusus
 sed -i 's/^bot_token\s*=\s*".*"/bot_token = "'"$new_token"'"/' /opt/autobackup/auto.py
 sed -i 's/^chat_id\s*=\s*".*"/chat_id = "'"$new_chatid"'"/' /opt/autobackup/auto.py
-sed -i 's/^name_server\s*=\s*".*"/name_server = "'"$new_name"'"/' /opt/autobackup/auto.py
+sed -i "s/NAME_SERVER/${NEW_NAME}/" /opt/autobackup/auto.py
 
 echo $new_token >> /etc/xray/token
 echo $new_chatid >> /etc/xray/chatid
