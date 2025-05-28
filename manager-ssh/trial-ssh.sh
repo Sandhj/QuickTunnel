adc55c94d41bba7d9eeca0ec3ae0f2c242dc096a
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-29 05:54:56 +0800

    Update trial-ssh.sh

--- manager-ssh/trial-ssh.sh
+++ manager-ssh/trial-ssh.sh
@@ -50,6 +50,11 @@ echo "Websocket : 80"
 echo "Websocket (TLS): 443"
 echo "BadVpn  : 7100-7900"
 echo "------------------------------------"
+echo "Websocket :"
+echo "$domain:80@$Login:$Pass"
+echo "Websocket TLS/SNI :"
+echo "$domain:443@$Login:$Pass"
+echo "------------------------------------"
 echo "Tekan Enter Untuk Kembali (↩️)"
 read -s
 menu
