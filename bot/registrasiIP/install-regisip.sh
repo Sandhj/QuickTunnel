24de5d38b82d6eff4ffec3c18bc17a8075c5e8e9
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-05-29 21:30:10 +0800

    Update install-regisip.sh

--- bot/registrasiIP/install-regisip.sh
+++ bot/registrasiIP/install-regisip.sh
@@ -9,7 +9,7 @@ wget -q https://raw.githubusercontent.com/Sandhj/QuickTunnel/main/bot/registrasi
 echo " SILAHKAN INPUT GITHUB TOKEN KAMU :"
 read -p " Token : " GITHUB
 
-sed -i "s/GITHUB_TOKEN/${GITHUB}/" /opt/regisip/regis.py
+sed -i "s/GITHUB_TOKEN_MU/${GITHUB}/" /opt/regisip/regis.py
 
 
 # Buat venv untuk Bot
