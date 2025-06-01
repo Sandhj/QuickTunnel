08b39515208662c67c82ccc12e527c748bcced6a
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-06-01 18:50:24 +0800

    Update auto_backup.sh

--- tools/auto_backup.sh
+++ tools/auto_backup.sh
@@ -6,7 +6,32 @@ mkdir -p /opt/autobackup
 cd /opt/autobackup
 
 # Buat venv untuk Bot
-apt install python3.12-venv -y
+# Fungsi untuk deteksi OS dan versi
+OS=$(grep -Ei '^(NAME|VERSION_ID)=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+# Ekstrak nama distro dan versi
+DISTRO=$(echo "$OS" | head -n1)
+VERSION=$(echo "$OS" | tail -n1)
+
+# Cek apakah Debian 12
+if [[ "$DISTRO" == "Debian GNU/Linux" && "$VERSION" == "12" ]]; then
+    echo "OS Terdeteksi: Debian 12"
+    apt update && apt install python3.11-venv -y
+
+# Cek apakah Ubuntu 24.04 LTS
+elif [[ "$DISTRO" == "Ubuntu" && "$VERSION" == "24.04" ]]; then
+    echo "OS Terdeteksi: Ubuntu 24.04 LTS"
+    apt update && apt install python3.12-venv -y
+
+# Jika bukan salah satu dari di atas
+else
+    echo "OS Tidak Didukung!"
+    echo "Distro: $DISTRO"
+    echo "Versi: $VERSION"
+    exit 1
+fi
+
+
 python3 -m venv bot
 source bot/bin/activate
 
