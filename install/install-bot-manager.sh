6dd4654076c715dcdbe147300c5cd6d95a75b963
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-06-19 17:57:34 +0800

    Update install-bot-manager.sh

--- install/install-bot-manager.sh
+++ install/install-bot-manager.sh
@@ -36,4 +36,23 @@ done
 # Hapus file dan direktori sementara
 rm -rf "$TMP_DIR" "$ZIP_FILE"
 
-echo "[+] Proses selesai. File telah dipindahkan ke $TARGET_DIR"
+# Minta input dari pengguna
+read -p "Masukkan token : " new_token
+read -p "Masukkan chatID : " new_chatid
+read -p "Perintah Identitas (contoh : server1) : " NEW_NAME
+
+# Ganti nilai di file Python dengan perlindungan karakter khusus
+sed -i 's/^bot_token\s*=\s*".*"/bot_token = "'"$new_token"'"/' /opt/autobackup/auto.py
+sed -i 's/^chat_id\s*=\s*".*"/chat_id = "'"$new_chatid"'"/' /opt/autobackup/auto.py
+sed -i "s/NAME_SERVER/${NEW_NAME}/" /opt/autobackup/auto.py
+
+echo $new_token >> /etc/xray/token
+echo $new_chatid >> /etc/xray/chatid
+
+systemctl restart auto
+
+echo " ✅ Autobackup Berhasil di Pasang. . .!"
+
+echo "Tekan Enter Untuk Menuju Menu Utama(↩️)"
+read -s
+menu
