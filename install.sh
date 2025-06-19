86202849e26eda7eb95a2da1d5b37d3748ea37cf
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-06-20 06:56:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -152,6 +152,43 @@ wget -q ${GITHUB}tools/auto_backup.sh && bash auto_backup.sh
 # ==== Install Swap 1GB
 wget -q ${GITHUB}tools/swap-1gb.sh && bash swap-1gb.sh
 
+# ==== Buat Vnv untuk keperluan Bot
+cd
+cd /opt
+
+OS=$(grep -Ei '^(NAME|VERSION_ID)=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+# Ekstrak nama distro dan versi
+DISTRO=$(echo "$OS" | head -n1)
+VERSION=$(echo "$OS" | tail -n1)
+
+# Cek apakah Debian 12
+if [[ "$DISTRO" == "Debian GNU/Linux" && "$VERSION" == "12" ]]; then
+    echo "OS Terdeteksi: Debian 12"
+    apt update && apt install python3.11-venv -y
+
+# Cek apakah Ubuntu 24.04 LTS
+elif [[ "$DISTRO" == "Ubuntu" && "$VERSION" == "24.04" ]]; then
+    echo "OS Terdeteksi: Ubuntu 24.04 LTS"
+    apt update && apt install python3.12-venv -y
+
+# Jika bukan salah satu dari di atas
+else
+    echo "OS Tidak Didukung!"
+    echo "Distro: $DISTRO"
+    echo "Versi: $VERSION"
+    exit 1
+fi
+
+
+python3 -m venv bot
+source bot/bin/activate
+apt-get install -y python3-pip
+
+# matikan Vnv
+deactivate
+cd
+
 # ==== Memasang Default Menu saat Boot
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
